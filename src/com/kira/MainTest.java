6561edfd3eaefe68d96b1720d0d9af82af936030
Kira-ZCX <798664894@qq.com>
2020-05-19 15:33:44 +0800

    update class

--- src/com/kira/MainTest.java
+++ src/com/kira/MainTest.java
@@ -3,6 +3,7 @@ package com.kira;
 import com.kira.common.utils.SortUtils;
 import com.kira.sort.InsertSort;
 import com.kira.sort.SelectSort;
+import com.kira.sort.ShellSort;
 
 public class MainTest {
 
@@ -16,7 +17,13 @@ public class MainTest {
         SortUtils.show(InsertSort.sort(a));
     }
 
+    public static void testShellSort() {
+        Integer[] a = {1,4,3,8,2,0,5,42,3,6,12,99,78,45,13,25,2};
+        SortUtils.show(ShellSort.sort(a));
+    }
+
     public static void main(String[] args) {
-        testInsertSort();
+        testShellSort();
+        //testInsertSort();
     }
 }
